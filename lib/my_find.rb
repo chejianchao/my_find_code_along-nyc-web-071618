5b762acce88c9b2988e0fdf00738d9e02389768e
chejianchao <820992376@qq.com>
2018-07-16 20:58:12 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,15 @@
 require 'pry'
 
 def my_find(collection)
-
+  i = 0
+  ans = nil
+  while i<collection.size
+    res = yield(collection[i])
+    if res == true
+      ans = collection[i]
+      break
+    end
+    i+=1
+  end
+  ans
 end
